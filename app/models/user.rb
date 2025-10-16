edb49923e3ba1910a08af2a86ffc37b7bf5cfb06
hzd4m <h.zd4m@gmail.com>
2025-10-16 19:42:26 -0300

    Sprint 1: Instalação e configuração do Devise

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: { editor: 0, admin: 1 }, _default: :editor
+
+  def admin?
+    role == 'admin'
+  end
+end
