244ac8e52cd9b0a39ab37ea3e8a931ff8d76b35d
hzd4m <h.zd4m@gmail.com>
2025-10-16 19:46:09 -0300

    Sprint 1 : Criação de Controllers(Hotel,Hospedes,Reservas)

---
+++ app/controllers/reservas_controller.rb
@@ -0,0 +1,3 @@
+class ReservasController < ApplicationController
+    before_action :authenticate_user!
+end
---
+++ test/controllers/hospedes_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class HospedesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
